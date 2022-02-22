34b7d21621ffee791e4d563995d5cad22e0c3240
Tanner <tannergames47@gmail.com>
2022-02-21 22:59:22 -0800

    added pages to the site

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/index')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
 
 
 if __name__ == '__main__':
