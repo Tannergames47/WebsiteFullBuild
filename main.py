4b880f54a40f511d96ca70e470bc49fb903895bd
Tanner <tannergames47@gmail.com>
2022-02-22 14:23:37 -0800

    created nav bar and block

--- main.py
+++ main.py
@@ -4,18 +4,34 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/index')
 def index():
     return render_template('index.html')
 
 
 
+# def base():
+#     name = "Tanner"
+#     return render_template('base.html', name = name)
+
+# @app.route('/home')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
+
+# dynamic routing
+# @app.route('/user/<username>')
+# def user(username):
+#     return f"This is the home page for {username.upper()}"
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
